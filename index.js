cac16a4c641eea8229c118c3fb82d309cbb9a76c
Jawad Ali <jawad.ali@repairdesk.co>
2025-02-12 17:37:43 +0500

    Fix service worker registration path in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("PWA is loading...");
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('./sw.js')
   .then(reg => console.log("Service Worker registered!", reg))
   .catch(err => console.log("Service Worker failed", err));
 }
