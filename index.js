f3452f2a7d8d61717dc84149e3a0a70b2440e794
Jawad Ali <jawad.ali@repairdesk.co>
2025-02-12 17:30:11 +0500

    Add PWA support with manifest and service worker implementation

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('ali');
+console.log("PWA is loading...");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(reg => console.log("Service Worker registered!", reg))
+  .catch(err => console.log("Service Worker failed", err));
+}
